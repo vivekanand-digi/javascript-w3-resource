691d5ce65eaa83b9f15befb607635857b55abd4b
vivekanand-digi <117620313+vivekanand-digi@users.noreply.github.com>
2023-09-11 12:44:18 +0530

    implemented spread operator

--- Basic/index.js
+++ Basic/index.js
@@ -532,4 +532,60 @@ let month = function (date) {
 };
 console.log(month(new Date("11/09/2023"))); //November
 
+//46)
+
+const person = [{ name: 'sai' }, { name: 'ashis' }, { name: 'vivek' }];
+for (const user of person) {
+    console.log(user.name); // sai ashis vivek
+}
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const evenNumbers = numbers.filter(function (number) {
+    return number % 2 === 0;
+});
+
+// OR
+const evenNumbers2 = numbers.filter((number) => number % 2 === 0);
+
+// Same with forEach
+const evenNumbers3 = [];
+numbers.forEach((number) => {
+    if (number % 2 === 0) {
+        evenNumbers.push(number);
+    }
+});
+
+const companies = [
+    { name: 'Company One', category: 'Finance', start: 2000, end: 2004 },
+    { name: 'Company Two', category: 'Retail', start: 2013, end: 2023 },
+    { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+    { name: 'Company Four', category: 'Retail', start: 1999, end: 2010 },
+
+];
+
+// Get only retail companies
+const retailCompanies = companies.filter(
+    (company) => company.category === 'Retail'
+);
+console.log(retailCompanies);
+
+// Get companies that started in or after 1980 and ended in or before 2005
+const earlyCompanies = companies.filter(
+    (company) => company.start >= 1980 && company.end <= 2005
+);
+console.log(earlyCompanies);
+
+// Get companies that lasted 10 years or more
+const longCompanies = companies.filter(
+    (company) => company.end - company.start >= 10
+);
+
+console.log(longCompanies);
+
+//spread operator
+let number = [1, 2, 3, 4];
+let newNumber = [...number, 5, 6, 7]
+console.log(newNumber) // [1,2,3,4,5,6,7]
 
